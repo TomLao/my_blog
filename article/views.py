5194c4ba7d4ea2dfb9aaacd53985d247563afe8e
tomlao <1026022953@qq.com>
2017-11-30 21:23:57 +0800

    主页添加分页显示，缩短每篇显示长度

--- article/views.py
+++ article/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger #添加包，分页
+
 #views用来处理程序逻辑, 然后呈现到template(一般为GET方法, POST方法略有不同)
 
 # Create your views here.
@@ -24,7 +26,15 @@ from django.http import Http404
 #     return render(request, 'test.html', {'current_time': datetime.now()})
 
 def home(request):
-    post_list=Article.objects.all() #获取全部的Article对象
+    posts=Article.objects.all() #获取全部的Article对象
+    paginator=Paginator(posts,5) #每页显示5个,一下主要修改主页上的显示，分页不要显示全部文章
+    page=request.GET.get('page')
+    try:
+        post_list=paginator.page(page)
+    except PageNotAnInteger:
+        post_list=paginator.page(1)
+    except EmptyPage:
+        post_list=paginator.paginator(paginator.num_pages)
     return render(request, 'home.html',{'post_list':post_list})
 
 #单条查找，id是博文的唯一标识，用id对数据库查找
